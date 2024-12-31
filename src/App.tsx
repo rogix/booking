f3458e292880a095890c184ae1b930696bdc746d
Rogerio Pereira <12768403+rogix@users.noreply.github.com>
2024-12-31 13:11:32 -0300

    chore: add shadcn and customize color

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
 	return (
 		<>
 			<div className="text-yellow-700">Hello</div>
+			<Button variant="default">Click me</Button>
 		</>
 	);
 }
