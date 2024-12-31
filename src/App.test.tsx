f1b1de34c4fe0c25e6c11748a4eb6b545f79bedb
Rogerio Pereira <12768403+rogix@users.noreply.github.com>
2024-12-31 12:46:49 -0300

    chore: install and setup tailwindcss

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,30 +3,10 @@ import { describe, expect, test } from "vitest";
 import App from "./App";
 
 describe("App component", () => {
-	test("renders Vite and React logos", () => {
+	test("renders Hello text", () => {
 		render(<App />);
-		const viteLogo = screen.getByAltText("Vite logo");
-		const reactLogo = screen.getByAltText("React logo");
-		expect(viteLogo).toBeInTheDocument();
-		expect(reactLogo).toBeInTheDocument();
-	});
-
-	test("renders Vite + React heading", () => {
-		render(<App />);
-		const heading = screen.getByText("Vite + React");
-		expect(heading).toBeInTheDocument();
-	});
-
-	test("renders initial count", () => {
-		render(<App />);
-		const button = screen.getByRole("button", { name: /count is 0/i });
-		expect(button).toBeInTheDocument();
-	});
-
-	test("increments count on button click", () => {
-		render(<App />);
-		const button = screen.getByRole("button", { name: /count is 0/i });
-		fireEvent.click(button);
-		expect(button).toHaveTextContent("count is 1");
+		const helloText = screen.getByText("Hello");
+		expect(helloText).toBeInTheDocument();
+		expect(helloText).toHaveClass("text-yellow-700");
 	});
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import path from "node:path";
 import { defineConfig as defineViteConfig, mergeConfig } from "vite";
 import { defineConfig as defineVitestConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 const viteConfig = defineViteConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
 
 const vitestConfig = defineVitestConfig({
