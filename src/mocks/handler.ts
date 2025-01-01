e204a86f9f161da2eddda4d8ce12064833450c25
Rogerio Pereira <12768403+rogix@users.noreply.github.com>
2025-01-01 20:25:10 -0300

    feat: add mocked data

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -4,30 +4,30 @@ import data from "./data.json";
 const API_BASE_URL = "http://mymock.com";
 
 export const handlers = [
-  http.get(
-    `${API_BASE_URL}/appointment_availabilities/available_times`,
-    ({ request }) => {
-      const url = new URL(request.url);
-      const startDateTime = url.searchParams.get("start_date_time");
-      const endDateTime = url.searchParams.get("end_date_time");
+	http.get(
+		`${API_BASE_URL}/appointment_availabilities/available_times`,
+		({ request }) => {
+			const url = new URL(request.url);
+			const startDateTime = url.searchParams.get("start_date_time");
+			const endDateTime = url.searchParams.get("end_date_time");
 
-      const mockData = {
-        ...data,
-      };
+			const mockData = {
+				...data,
+			};
 
-      const filteredTimes = mockData.data.available_times.filter((time) => {
-        const timeDate = new Date(time);
-        return (
-          (!startDateTime || timeDate >= new Date(startDateTime)) &&
-          (!endDateTime || timeDate <= new Date(endDateTime))
-        );
-      });
+			const filteredTimes = mockData.data.available_times.filter((time) => {
+				const timeDate = new Date(time);
+				return (
+					(!startDateTime || timeDate >= new Date(startDateTime)) &&
+					(!endDateTime || timeDate <= new Date(endDateTime))
+				);
+			});
 
-      return HttpResponse.json({
-        data: {
-          available_times: filteredTimes,
-        },
-      });
-    }
-  ),
+			return HttpResponse.json({
+				data: {
+					available_times: filteredTimes,
+				},
+			});
+		},
+	),
 ];
