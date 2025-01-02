8ff3cb2818adf16e39d888c332a9f15c5e043609
Rogerio Pereira <12768403+rogix@users.noreply.github.com>
2025-01-02 00:50:49 -0300

    feat: use react hook form

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,55 +1,69 @@
-import { useState } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import { Button } from "./ui/button";
 
 interface UserFormProps {
 	onSubmit: (data: { name: string; email: string }) => void;
 }
 
+interface UserFormInputs {
+	name: string;
+	email: string;
+}
+
 const UserForm: React.FC<UserFormProps> = ({ onSubmit }) => {
-	const [formData, setFormData] = useState({ name: "", email: "" });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+	} = useForm<UserFormInputs>({
+		mode: "onChange",
+	});
 
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setFormData({ ...formData, [e.target.name]: e.target.value });
-	};
-
-	const handleSubmit = () => {
-		if (formData.name && formData.email) {
-			onSubmit(formData);
-		}
+	const onSubmitForm: SubmitHandler<UserFormInputs> = (data) => {
+		onSubmit(data);
 	};
 
 	return (
 		<section className="p-4 flex-1">
 			<h2 className="text-2xl font-semibold">Enter details</h2>
-			<div className="flex flex-col gap-4 w-[400px]">
+			<form
+				onSubmit={handleSubmit(onSubmitForm)}
+				className="flex flex-col gap-4 w-[400px]"
+			>
 				<div className="flex flex-col gap-2">
 					<label htmlFor="name">Name</label>
 					<input
 						className="border border-gray-300 rounded-md p-2 w-[400px]"
-						name="name"
+						id="name"
+						{...register("name", { required: "Name is required" })}
 						placeholder="Name"
-						value={formData.name}
-						onChange={handleChange}
 					/>
+					{errors.name && (
+						<p className="text-red-500 text-sm">{errors.name.message}</p>
+					)}
 				</div>
 				<div className="flex flex-col gap-2">
 					<label htmlFor="email">Email</label>
 					<input
 						className="border border-gray-300 rounded-md p-2 w-[400px]"
-						name="email"
+						id="email"
+						{...register("email", {
+							required: "Email is required",
+							pattern: {
+								value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+								message: "Invalid email address",
+							},
+						})}
 						placeholder="Email"
-						value={formData.email}
-						onChange={handleChange}
 					/>
+					{errors.email && (
+						<p className="text-red-500 text-sm">{errors.email.message}</p>
+					)}
 				</div>
-				<Button
-					type="button"
-					onClick={handleSubmit}
-					disabled={!formData.name || !formData.email}
-				>
+				<Button type="submit" disabled={!isValid}>
 					Schedule Event
 				</Button>
-			</div>
+			</form>
 		</section>
 	);
 };
